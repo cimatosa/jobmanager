48831754f8522f6cabf466acf585fc990a40031e
cimatosa <cimatosa@gmx.de>
2015-01-21 14:45:41 +0100

    catch persistentData import Error when sqlitedict is not there

--- jobmanager/__init__.py
+++ jobmanager/__init__.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import warnings
+
 from .jm_version import __version__
 
 from .jobmanager import *
 
 from . import clients
 from . import decorators
-from . import servers
 from . import progress
-from . import persistentData
+from . import servers
+
+# persistentData requires sqlitedict
+try:
+    from . import persistentData
+except ImportError as e:
+    warnings.warn("Submodule 'persistentData' is not available. Reason: {}.".format(e))
 
 # ode_wrapper requires scipy
 try:
     from . import ode_wrapper
 except ImportError:
-    warnings.warn("Submodule 'ode_wrapper' is not available."+\
-                  " Reason: {}.".format(sys.exc_info()[1].message))
+    warnings.warn("Submodule 'ode_wrapper' is not available. Reason: {}.".format(e))
