a23b2d4bbb5ddd9ece7f0c6d56a7f498db642c71
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2015-04-03 09:17:26 +0200

    "automated doc upload"

---
+++ _downloads/simple_example.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function
+
+import multiprocessing as mp
+import numpy as np
+from os.path import split, dirname, abspath
+import sys
+import time
+
+# Add parent directory to beginning of path variable
+sys.path = [split(dirname(abspath(__file__)))[0]] + sys.path
+
+import jobmanager as jm
+
+
+
+class Example_Client(jm.JobManager_Client):
+    def __init__(self):
+        # start quiet client (verbopse=0)
+        super(Example_Client, self).__init__(server="localhost", 
+                         authkey='simple example', 
+                         verbose=0)
+        
+    @staticmethod
+    def func(args, const_args):
+        """simply return the current argument"""
+        return args
+
+        
+
+class Example_Server(jm.JobManager_Server):
+    def __init__(self):
+        # server show status information (verbose=1)
+        super(Example_Server, self).__init__(authkey='simple example',
+                         verbose=1)
+
+        self.final_result = 1
+            
+        
+    def process_new_result(self, arg, result):
+        """over write final_result with the new incoming result 
+        if the new result is smaller then the final_result""" 
+        if self.final_result > result:
+            self.final_result = result
+            
+    def process_final_result(self):
+        print("final_result:", self.final_result)
+        
+
+
+def run_server():
+    with Example_Server() as server:
+        for i in range(5000):
+            server.put_arg(np.random.rand())
+        server.start()
+    
+    
+def run_client():
+    client = Example_Client()
+    client.start()
+
+
+if __name__ == "__main__":
+    p_server = mp.Process(target=run_server)
+    p_server.start()
+    
+    time.sleep(1)
+    
+    p_client = mp.Process(target=run_client)
+    p_client.start()
+
+    p_client.join()
+    p_server.join()
---
+++ _sources/index.txt
@@ -0,0 +1,87 @@
+jobmanager documentation
+========================
+
+
+Content
+-------
+.. toctree::
+   :maxdepth: 4
+   
+   index
+    
+For an alphabetically sorted list of all modules, classes, and 
+methods, see :ref:`genindex`.
+
+
+Main module
+-----------
+.. automodule:: jobmanager
+
+
+Progress monitoring
+-------------------
+.. automodule:: jobmanager.progress
+   :members:
+
+
+User Convenience
+-----------------
+Decorators
+~~~~~~~~~~
+.. automodule:: jobmanager.decorators
+   :members:
+   
+   
+ODE wrapper
+~~~~~~~~~~~
+.. automodule:: jobmanager.ode_wrapper
+   :members:
+
+
+Core modules
+------------
+jobmanager
+~~~~~~~~~~
+clients
+~~~~~~~
+.. automodule:: jobmanager.jobmanager
+   :members:
+
+clients
+~~~~~~~
+.. automodule:: jobmanager.clients
+   :members:
+
+
+servers
+~~~~~~~
+.. automodule:: jobmanager.servers
+   :members:
+
+
+persistentData
+~~~~~~~~~~~~~~
+.. automodule:: jobmanager.persistentData
+   :members:
+
+
+
+Examples
+--------
+
+simple example
+~~~~~~~~~~~~~~
+.. automodule:: simple_example
+Download: :download:`../examples/simple_example.py`.
+
+.. literalinclude:: ../examples/simple_example.py
+
+
+decorators
+~~~~~~~~~~
+.. automodule:: wrapper_example
+Download: :download:`../examples/wrapper_example.py`.
+
+.. literalinclude:: ../examples/wrapper_example.py
+
+
